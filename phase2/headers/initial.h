2ec18e3fb345a6991bcf5646ad643b53235ee8cc
Salvatore Bruzzese <salvatorebruzzese05@gmail.com>
2026-02-25 17:29:59 +0100

    add: implementazione parziale dell'inizializzazione del nucleo

--- phase2/headers/initial.h
+++ phase2/headers/initial.h
@@ -1,3 +1,13 @@
 #ifndef INITIAL_H
 #define INITIAL_H
+
+#include "../../headers/types.h"
+
+typedef struct {
+    memaddr tlb_refill_handler;
+    memaddr tlb_stack_pointer;
+    memaddr exception_handler;
+    memaddr exception_stack_pointer;
+} ppv_t;
+
 #endif
--- phase2/initial.c
+++ phase2/initial.c
@@ -1,1 +1,17 @@
 #include "headers/initial.h"
+#include "../headers/types.h"
+#include "../phase1/headers/asl.h"
+
+int main() {
+    unsigned int process_count;
+    unsigned int soft_block_count;
+    LIST_HEAD(ready_queue);
+    pcb_PTR running_pcb;
+
+    ppv_t * pass_up_vector = (ppv_t *) 0x0FFFF900;
+
+    pass_up_vector->tlb_refll_handler = (memaddr)uTLB_RefillHandler;
+    pass_up_vector->tlb_stack_pointer = (memaddr)KERNELSTACK;
+    pass_up_vector->exception_handler = (memaddr)exceptionHandler;
+    pass_up_vector->exception_stack_pointer = (memaddr)KERNELSTACK;
+}
