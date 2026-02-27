7df01bed2f1930d11e0e97c065b162a55c57f9b3
raaaandom <itzraandom@gmail.com>
2026-02-27 11:40:56 +0100

    add: user-mode and kernel-mode handling

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -1,8 +1,8 @@
 #include "headers/exceptions.h"
 #include <uriscv/liburiscv.h>
 #include "../headers/const.h"
-#include "../headers/types.h"
 #include "headers/interrupts.h"
+#include "uriscv/types.h"
 
 // macro to determine if an exception is an interrupt or not
 // works by checking the MSB of the passed register
@@ -11,7 +11,31 @@
 #define CAUSE_CODE(cause) ((cause) & GETEXECCODE)
 
 // syscalls sub-handler
-static void _syscallHandler() {}
+static void _syscallHandler() {
+
+    // get privileges
+    int mode = GET_EXCEPTION_STATE_PTR(0)->status & MSTATUS_MPP_MASK;
+
+    // privileged syscall requested
+    if(GET_EXCEPTION_STATE_PTR(0)->reg_a0 < 0) {
+
+        if(mode != MSTATUS_MPP_MASK) { // review needed
+
+            // syscall not authorized, send a trap
+
+        } else {
+
+            // all good, proceed
+
+        }
+
+    } else {
+
+        // non privileged syscall requested, proceed
+
+    }
+
+}
 
 // pass up or die sub-handler
 static void _puodHandler(int idx) {}
