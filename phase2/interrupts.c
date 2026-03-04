61857f91a97e99a8a7e8a1bc3bfcf56228c382e7
William Brusa <william.brusa@studio.unibo.it>
2026-03-04 11:19:30 +0100

    chore: format

--- phase2/interrupts.c
+++ phase2/interrupts.c
@@ -1,7 +1,7 @@
 #include "headers/interrupts.h"
 
-#include <uriscv/liburiscv.h>
 #include <uriscv/arch.h>
+#include <uriscv/liburiscv.h>
 #include <uriscv/types.h>
 
 #include "../headers/const.h"
@@ -37,41 +37,40 @@ void interruptHandler() {
     }
 }
 
-
 static void nonTimerInterrupt(int intLineNo) {
-  cpu_t devNo = -1;
+    cpu_t devNo = -1;
 
-  // decreasing priority: 0x10000040 (l3) .. 0x10000040+0x10 (l7)
-  cpu_t* devMaskAddr = (cpu_t *) CDEV_BITMAP_ADDR(intLineNo);
-  for (cpu_t b = 0; b < N_DEV_PER_IL; b++) {
-    if ((*devMaskAddr >> b) & 1) { // NOTE: check right-most first
-      devNo = b;
-      break;
+    // decreasing priority: 0x10000040 (l3) .. 0x10000040+0x10 (l7)
+    cpu_t* devMaskAddr = (cpu_t*)CDEV_BITMAP_ADDR(intLineNo);
+    for (cpu_t b = 0; b < N_DEV_PER_IL; b++) {
+        if ((*devMaskAddr >> b) & 1) { // NOTE: check right-most first
+            devNo = b;
+            break;
+        }
     }
-  }
 
-  if (devNo == -1) // No pending interrupt
-    return;
+    if (devNo == -1) // No pending interrupt
+        return;
 
-  dtpreg_t* devAddr = (dtpreg_t*) DEV_REG_ADDR(intLineNo, devNo);
+    dtpreg_t* devAddr = (dtpreg_t*)DEV_REG_ADDR(intLineNo, devNo);
 
-  cpu_t status = devAddr->status;
-  devAddr->command = ACK; // acknowledge
+    cpu_t status = devAddr->status;
+    devAddr->command = ACK; // acknowledge
 
-  // NOTE: Using ASL, with device register addr as key for asl list
-  // TODO: decide device asl addresses
-  pcb_t* proc = removeBlocked((int*)devAddr); // spec 7.5.4
-  if (proc == NULL)
-    return;
-  proc->p_s.reg_a0 = status; // spec 7.5.5
-  insertProcQ(&ready_queue, proc); // spec 7.5.6
-  LDST(&proc->p_s); // spec 7.5.7
+    // NOTE: Using ASL, with device register addr as key for asl list
+    // TODO: decide device asl addresses
+    pcb_t* proc = removeBlocked((int*)devAddr); // spec 7.5.4
+    if (proc == NULL)
+        return;
+    proc->p_s.reg_a0 = status;       // spec 7.5.5
+    insertProcQ(&ready_queue, proc); // spec 7.5.6
+    LDST(&proc->p_s);                // spec 7.5.7
 }
 
 static void localTimerInterrupt() {
-  // spec 7.2
-  setTIMER(TIMESLICE);
-  STST(&(running_pcb->p_s));
-  insertProcQ(&ready_queue, running_pcb);
-  // FIXME: call scheduler()
+    // spec 7.2
+    setTIMER(TIMESLICE);
+    STST(&(running_pcb->p_s));
+    insertProcQ(&ready_queue, running_pcb);
+    // FIXME: call scheduler()
 }
