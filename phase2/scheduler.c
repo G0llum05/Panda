40b853537424e034194891943694ad0d709dc7b4
Mattia Graziani <mattia.graziani3@studio.unibo.it>
2026-03-12 09:44:20 +0100

    add: v1 scheduler after recovery

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -1,3 +1,25 @@
 #include "headers/scheduler.h"
 
-void scheduler() {}
+#include "uriscv/liburiscv.h"
+#include <uriscv/types.h>
+
+#include "../headers/const.h"
+#include "../phase1/headers/pcb.h"
+
+#include "headers/initial.h"
+
+void scheduler() {
+    if (!emptyProcQ(&ready_queue)) {
+        running_pcb = removeProcQ(&ready_queue);
+        setTIMER(TIMESLICE);
+        LDST(&running_pcb->p_s);
+    } else {
+        if (process_count == 0) {
+            HALT();
+        } else if (process_count > 0 && soft_block_count == 0) {
+            WAIT();
+        } else {
+            PANIC();
+        }
+    }
+}
