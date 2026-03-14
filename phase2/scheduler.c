a5668807992a692957093bcd610775b479fea5db
Salvatore Bruzzese <salvatorebruzzese05@gmail.com>
2026-03-14 12:45:29 +0100

    fix: swapped HALT() and PANIC()
    The processor should panic only on deadlock and wait otherwise. Added a comment to make the deadlock check clearer.

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -13,13 +13,14 @@ void scheduler() {
         running_pcb = removeProcQ(&ready_queue);
         setTIMER(TIMESLICE);
         LDST(&running_pcb->p_s);
-    } else {
-        if (process_count == 0) {
-            HALT();
-        } else if (process_count > 0 && soft_block_count == 0) {
-            WAIT();
-        } else {
-            PANIC();
-        }
-    }
+    } else if (process_count == 0)
+        // When all processes are terminated we shut down
+        HALT();
+    else if (process_count > 0 && soft_block_count == 0)
+        // This check is logically sound because time is
+        // quantized using clock interrupts; if no process is
+        // waiting for the clock or I/O then there's a deadlock.
+        PANIC();
+    else
+        WAIT();
 }
