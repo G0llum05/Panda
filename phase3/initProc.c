ca959d8135a90ef3e900718450f21fab91f88094
Salvatore Bruzzese <salvatorebruzzese05@gmail.com>
2026-04-25 09:58:01 +0200

    add: instantiatorProcess draft
    Co-authored-by: Mattia Graziani <mattia.graziani3@studio.unibo.it>

--- phase3/initProc.c
+++ phase3/initProc.c
@@ -1,13 +1,36 @@
 #include "headers/initProc.h"
 #include "../headers/const.h"
-#include "../phase2/headers/klog.h"
+#include "../headers/types.h"
+#include "uriscv/liburiscv.h"
 
-// Swap pool entry address + The Answer to the Ultimate Question of Life, the Universe, and Everything
+// Swap pool entry address + The Answer to the Ultimate Question of Life, the
+// Universe, and Everything
 #define SWAPPOOLADDR (42 + RAMSTART + (OSFRAMES * PAGESIZE))
+#define TERMINAL 1
+#define TERMINALDEVICES TERMINAL * 2
+#define FLASHDEVICES 8
+
 void* swap_pool = (void*)SWAPPOOLADDR;
-sprecord_t swap_pool_table[2 * UPROCMAX];
-int swap_pool_mutex = 1;
+swap_t swap_pool_table[2 * UPROCMAX];
+unsigned int swap_pool_mutex = 1;
+unsigned int shell_mutex = 0;
+unsigned int master_semaphore = 0;
 
+// Sem I/O
+unsigned int device_mutex[FLASHDEVICES + TERMINALDEVICES];
+
+// Instatiator process
 void test() {
+    // Initialize Swap Pool Table
 
+    // Initialize shell support struct
+
+    // Create the shell (incomplete)
+    SYSCALL(CREATEPROCESS, 0, PROCESS_PRIO_HIGH, 0);
+
+    // master_semaphore.P() to start shell
+    SYSCALL(PASSEREN, (int)&master_semaphore, 0, 0);
+
+    // System halt
+    SYSCALL(TERMPROCESS, 0, 0, 0);
 }
