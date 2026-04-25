672415ab0c2b781ed4c92434472f36d9eca5d5cf
Salvatore Bruzzese <salvatorebruzzese05@gmail.com>
2026-04-25 09:59:39 +0200

    add: TLB management helper comments
    Co-authored-by: Mattia Graziani <mattia.graziani3@studio.unibo.it>

--- phase3/vmSupport.c
+++ phase3/vmSupport.c
@@ -7,3 +7,22 @@ void uTLB_RefillHandler() {
     TLBWR();
     LDST((state_t*)BIOSDATAPAGE);
 }
+
+/*
+    Spec 5.3:
+
+    Updating the page table of a process requires
+    also updating the associated tlb entry in the tlb.
+    Either:
+    1. Delete all the TLB
+    2. Probe the TLB and rewrite the entry if present
+
+    IMPORTANT: to update the pager atomically we must
+    first disable interrupts and the enable them back.
+    disable: setSTATUS(getSTATUS() & ~MSTATUS_MIE_MASK)
+    enable: setSTATUS(getSTATUS() | MSTATUS_MIE_MASK))
+*/
+
+// Spec 5.4:
+// The pager algorithm is FIFO and uses a static variable
+// modulo the size of the swap pool (round robin)
