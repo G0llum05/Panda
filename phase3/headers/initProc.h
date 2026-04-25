fe9744ad7636beb7f7f37d9eebf3ed198299de4a
Salvatore Bruzzese <salvatorebruzzese05@gmail.com>
2026-04-25 09:46:07 +0200

    fix: redundant struct definition
    The page table entry and swap pool entry descriptors are already defined in
    headers/types.h. It is not needed to redefine them, as /usr/include/uriscv/cpu.h
    defines proper entryHi and entryLo bit masks and get bits macros.
    
    Co-authored-by: Mattia Graziani <mattia.graziani3@studio.unibo.it>

--- phase3/headers/initProc.h
+++ phase3/headers/initProc.h
@@ -1,31 +1,4 @@
 #ifndef INITPROC_H
 #define INITPROC_H
 
-typedef struct {
-    union {
-        unsigned int data;
-        struct {
-            unsigned int asid;
-            unsigned int vpn : 4;
-        } bit;
-    } entryHi;
-    union {
-        unsigned int data;
-        struct {
-            unsigned int N : 1;
-            unsigned int D : 2;
-            unsigned int V : 3;
-            unsigned int G : 4;
-            unsigned int pfn;
-        } bit;
-    } entryLo;
-} pgrecord_t;
-
-// sprecord := Swap Pool table record
-typedef struct sprecord {
-    int ASID;
-    int VPN;
-    pgrecord_t* page;
-} sprecord_t;
-
 #endif
