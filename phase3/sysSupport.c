056b49a5bcf4f1fa46b4615d0514b4a9a7624c1a
Salvatore Bruzzese <salvatorebruzzese05@gmail.com>
2026-04-26 12:19:42 +0200

    add: exception codes to switch

--- phase3/sysSupport.c
+++ phase3/sysSupport.c
@@ -1,15 +1,37 @@
 #include "headers/sysSupport.h"
-
+#include "headers/vmSupport.h"
 #include "uriscv/cpu.h"
 #include "uriscv/liburiscv.h"
 
-static void supportExceptionHandler() {
-    // get cause of support structure
+static void supportSyscallHandler();
+static void programTrapHandler();
+
+void supportExceptionHandler() {
+    // REVIEW: does this function automatically get the
+    // cause from the correct exception state?
+    // It is important, otherwise we don't know where
+    // to route the exception in the switch.
     unsigned int cause = getCAUSE();
 
-    // switch (cause) {
-    // // case EXC_MOD ... EXC_UTLBS: pager();
-    // // case PROGRAM TRAP: programTrapHandler();
-    // // case SYSCALL: supportSyscallHandler();
-    // }
+    switch (cause) {
+    case EXC_TLBL ... EXC_UTLBS:
+        pager();
+        break;
+
+    // REVIEW: Environment Calls in Machine mode should not be
+    // processed by the support level exception handler
+    case EXC_ECU:
+        supportSyscallHandler();
+        break;
+
+    // Spec 4.2: "[...] TLB-modification exceptions should not occur.
+    // If they do, they should be treated as a program trap."
+    default:
+        programTrapHandler();
+        break;
+    }
 }
+
+static void supportSyscallHandler() {}
+
+static void programTrapHandler() {}
