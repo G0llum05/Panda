1ff850e0f9b20c5daca04d2622b9f4b70270629f
William Brusa <william.brusa@studio.unibo.it>
2026-05-12 14:53:12 +0200

    fix: better shell implementation

--- testers/shell.c
+++ testers/shell.c
@@ -1,35 +1,19 @@
+#include "h/print.h"
 #include "h/string.h"
 #include "h/tconst.h"
 #include <uriscv/liburiscv.h>
 
-// NOTE: first implemented function MUST be main
-static void _exit();
-static void _unknown();
-
-#define SML 3
-
 void main() {
-    const char fibEight[] = "fibEight";
-    const char echo[] = "echo";
-    const char fibEleven[] = "fibEleven";
-    const char uname[] = "uname";
-    const char date[] = "date";
-    const char sl[] = "sl";
-    const char calc[] = "calc";
-    const char exit[] = "exit";
-    const char* commands[7] = {fibEight, echo, fibEleven, uname,
-                               date,     sl,   calc};
-
+    const char* commands[7] = {"fibEight", "echo", "fibEleven", "uname",
+                               "date",     "sl",   "calc"};
     char ibuffer[128];
-    char shell_mark[SML] = ">> ";
-    int status = SYSCALL(WRITETERMINAL, (int)shell_mark, SML, 0);
-    if (status < 0)
-        _exit();
+    char* shell_mark = "|> ";
+    print(WRITETERMINAL, shell_mark);
 
     while (1) {
         const int ilen = SYSCALL(READTERMINAL, (int)ibuffer, 0, 0);
         if (ilen < 0)
-            _exit();
+            SYSCALL(TERMINATE, 0, 0, 0);
 
         char token[ilen];
         int found = 0;
@@ -43,24 +27,14 @@ void main() {
             }
         }
 
-        if (strcmp(token, exit) == 0) {
-            _exit();
+        if (strcmp(token, "exit") == 0) {
+            print(WRITETERMINAL, "Goodbye, and thanks for all the fish!\n");
+            SYSCALL(TERMINATE, 0, 0, 0);
         }
 
         if (!found)
-            _unknown();
-
-        status = SYSCALL(WRITETERMINAL, (int)shell_mark, SML, 0);
-        if (status < 0)
-            _exit();
+            print(WRITETERMINAL, "Unknown instruction.\n");
+        print(WRITETERMINAL, shell_mark);
     }
-}
-static void _exit() { SYSCALL(TERMINATE, 0, 0, 0); }
-
-static void _unknown() {
-    // FIXME: can't print this shit, out of memory?
-    /* const char unknown[] = "Unknown instruction\n"; */
-    /* int status = SYSCALL(WRITETERMINAL, (int)unknown, 20, 0); */
-    /* if (status < 0) */
-    /*     _exit(); */
+    SYSCALL(TERMINATE, 0, 0, 0);
 }
